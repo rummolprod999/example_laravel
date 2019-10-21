2aa61791ab72cc7897c043c539c14f1c118f2fc1
rummolprod999 <rummolprod999@gmail.com>
2019-10-21 10:12:32 +0300

    update

--- database/migrations/2019_10_21_070346_change_tasks_table.php
+++ database/migrations/2019_10_21_070346_change_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class ChangeTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('notice')->change();
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('notice');
         });
     }
 
